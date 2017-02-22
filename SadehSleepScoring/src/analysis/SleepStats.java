e92569fa5dab82560a994d89be817b5a6fcad603
Kyle Moser <moser.kyle@gmail.com>
2017-02-21 19:20:43 -0500

    sleep onset and sleep offset algorithms to find what time a person fell asleep and woke up, based on the known sleep periods that were calculated previously.

--- SadehSleepScoring/src/analysis/SleepStats.java
+++ SadehSleepScoring/src/analysis/SleepStats.java
@@ -13,30 +13,45 @@ public class SleepStats {
 	LocalDateTime sleepOnset;
 	LocalDateTime sleepOffset;
 	
+	/**
+	 * Colloquially, the sleep onset is the time a person initially fell asleep on a particular date.
+	 * This method finds the sleep onset as follows. Find an epoch coded as asleep between 8PM and 12:00AM.
+	 * If none is found, find one between 6PM and 12:00AM. If still none is found, between 6PM and 8:00AM.
+	 * (AM times are obviously the following day; PM times are the same day. First the algorithm works backwards,
+	 * assuming that the person may have fallen asleep early in the evening; then, the algorithm expands the 
+	 * search to include a larger portion of the following day).
+	 * 
+	 * @param forDate Find a sleep onset for this date
+	 * @param sleepPeriods Sleep onset is the first epoch in one of these sleep periods
+	 * @return
+	 */
 	public static SleepPeriod findSleepOnset(LocalDate forDate, List<SleepPeriod> sleepPeriods){
 		LocalDateTime ldtStartInterval = forDate.atTime(19, 59, 59);
 		LocalDateTime ldtEndInterval = forDate.plusDays(1).atTime(0,0,1);
 		SortedSet<SleepPeriod> spSet = new TreeSet<>(new SleepPeriodSorter());
 		spSet.addAll(sleepPeriods);
 		
+		//Check to find the first sleep period that started between the date at 8PM and the next morning at 12:00AM
 		for (SleepPeriod sp : spSet){
-			if (between(ldtStartInterval, ldtEndInterval, sp)){
+			if (between(ldtStartInterval, ldtEndInterval, sp.getStart())){
 				return sp;
 			}
 		}
 		
 		ldtStartInterval = forDate.atTime(17, 59, 59);
 		
+		//Check to find the first sleep period that started between the date at 6PM and the next morning at 12:00AM
 		for (SleepPeriod sp : spSet){
-			if (between(ldtStartInterval, ldtEndInterval, sp)){
+			if (between(ldtStartInterval, ldtEndInterval, sp.getStart())){
 				return sp;
 			}
 		}
 		
 		ldtEndInterval = forDate.plusDays(1).atTime(8,0,1);
 		
+		//Check to find the first sleep period that started between the date at 6PM and the next morning at 8AM
 		for (SleepPeriod sp : spSet){
-			if (between(ldtStartInterval, ldtEndInterval, sp)){
+			if (between(ldtStartInterval, ldtEndInterval, sp.getStart())){
 				return sp;
 			}
 		}
@@ -50,25 +65,26 @@ public class SleepStats {
 		TreeSet<SleepPeriod> spSet = new TreeSet<>(new SleepPeriodSorter());
 		spSet.addAll(sleepPeriods);
 		
-		//The sleep onset and sleep offset times can potentially overlap; and obviously, a person
-		//cannot fall asleep after (or at the same time) they wake up. Therefore, sleep periods before
-		//the sleep onset are removed, so the sleep offset will definitely be after the onset.
+		//A person cannot fall asleep after they wake up so we filter impossible times from the search set
 		Iterator<SleepPeriod> matchIt = spSet.descendingIterator();
 		boolean foundOnset = false;
 		while (matchIt.hasNext()){
 			SleepPeriod sp = matchIt.next();
-			if (sp.equals(onset))
-				foundOnset = true;
 			
 			if (foundOnset)
 				matchIt.remove();
+			
+			//The person fell asleep during this interval
+			if (sp.equals(onset))
+				foundOnset = true;
 		}
 		
 		Iterator<SleepPeriod> reverseIt = spSet.descendingIterator();
 		
+		//See if the person woke up the next day from 6AM to 9AM
 		while (reverseIt.hasNext()){
 			SleepPeriod sp = reverseIt.next();
-			if (between(ldtStartInterval, ldtEndInterval, sp)){
+			if (between(ldtStartInterval, ldtEndInterval, sp.getEnd())){
 				return sp;
 			}
 		}
@@ -76,19 +92,21 @@ public class SleepStats {
 		reverseIt = spSet.descendingIterator();
 		ldtStartInterval = forDate.plusDays(1).atTime(3, 59, 59);
 		
+		//See if the person woke up the next day from 4AM to 9AM
 		while (reverseIt.hasNext()){
 			SleepPeriod sp = reverseIt.next();
-			if (between(ldtStartInterval, ldtEndInterval, sp)){
+			if (between(ldtStartInterval, ldtEndInterval, sp.getEnd())){
 				return sp;
 			}
 		}
 		
 		reverseIt = spSet.descendingIterator();
-		ldtEndInterval = forDate.plusDays(1).atTime(9,0,1);
+		ldtEndInterval = forDate.plusDays(1).atTime(10,0,1);
 		
+		//See if the person woke up the next day from 4AM to 10AM
 		while (reverseIt.hasNext()){
 			SleepPeriod sp = reverseIt.next();
-			if (between(ldtStartInterval, ldtEndInterval, sp)){
+			if (between(ldtStartInterval, ldtEndInterval, sp.getEnd())){
 				return sp;
 			}
 		}
@@ -96,10 +114,9 @@ public class SleepStats {
 		return null;
 	}
 	
-	public static boolean between(LocalDateTime start, LocalDateTime end, SleepPeriod sp){
-		LocalDateTime sleepPeriodStart = sp.getStart();
-		boolean isBefore = start.isBefore(sleepPeriodStart);
-		boolean isAfter = end.isAfter(sleepPeriodStart);
+	public static boolean between(LocalDateTime start, LocalDateTime end, LocalDateTime sp){
+		boolean isBefore = start.isBefore(sp);
+		boolean isAfter = end.isAfter(sp);
 		return isBefore && isAfter;
 	}
 	
